f4d88c343b6fcb493eaced993a65814c968680de
cristianblar <cristian@cristianblar.dev>
2022-01-31 13:43:35 -0500

    feat: add Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Box, Typography } from '@mui/material'
 export default function Header() {
   return (
     <Box p={2}>
-      <Typography width="100%" textAlign="center" variant="h1">
+      <Typography width="100%" textAlign="center" fontSize="60px" variant="h1">
         Las Vegas Casino
       </Typography>
     </Box>
