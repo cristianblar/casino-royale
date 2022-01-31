6015829c76d65e514d4877833ca7c78617e7b170
cristianblar <cristian@cristianblar.dev>
2022-01-31 06:48:47 -0500

    chore: add useSession custom hook

---
+++ src/hooks/useSession.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+import { BACKEND_URL } from 'constant'
+
+export default function useSession() {
+  const [loading, setLoading] = useState(false)
+  const [authenticated, setAuthenticated] = useState(false)
+
+  useEffect(() => {
+    async function checkStatus() {
+      const checkFetch = await fetch(`${BACKEND_URL}/auth/check-status`)
+      console.log(checkFetch)
+      if (checkFetch.ok && !authenticated) setAuthenticated(true)
+      if (!checkFetch.ok && authenticated) setAuthenticated(false)
+    }
+    setLoading(true)
+    checkStatus().then(() => setLoading(false))
+  }, [authenticated])
+
+  return { loading, authenticated, changeAuthStatus: setAuthenticated }
+}
